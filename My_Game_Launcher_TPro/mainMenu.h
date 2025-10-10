e80d941f8f6e31b8234a8c262cf7e4ddf1c1f309
Максим Колмогоров <maksim28062006@mail.ru>
2025-10-10 20:17:13 +0300

    Исправил замечания

---
+++ My_Game_Launcher_TPro/mainMenu.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <cmath> // для sin
+
+class MenuItem;
+
+
+class MenuItem {
+public:
+    sf::Text text;
+    std::function<void()> onClick;
+    bool hovered = false;
+    MenuItem(const sf::String& label, sf::Font& font, unsigned int size, const sf::Vector2f& pos, std::function<void()> callback);
+    bool isMouseOver(const sf::RenderWindow& window) const;
+    void update(float time);
+};
