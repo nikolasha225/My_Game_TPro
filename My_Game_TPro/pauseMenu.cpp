0f814406696938138413b4d23e3cd33cc83c7297
Максим Колмогоров <maksim28062006@mail.ru>
2025-11-06 19:56:05 +0300

    правки меню паузы, выглядит лучше

--- My_Game_TPro/pauseMenu.cpp
+++ My_Game_TPro/pauseMenu.cpp
@@ -45,40 +45,51 @@ void renderPause(sf::RenderWindow* window, EnumGameState& gameState) {
         std::cerr << "Не удалось загрузить шрифт!\n";
         return;
     }
+
+    //для заднего фона
+    sf::Texture gameScreenshot;
+    gameScreenshot.create(window->getSize().x, window->getSize().y);
+    gameScreenshot.update(*window);
+    sf::Sprite backgroundSprite(gameScreenshot);
+
+    //просто по красоте
     sf::RectangleShape overlay(sf::Vector2f(window->getSize()));
     overlay.setFillColor(sf::Color(0, 0, 0, 150));
 
+    sf::RectangleShape menuBackground(sf::Vector2f(600.f, 400.f));
+    menuBackground.setFillColor(sf::Color(20, 20, 30, 200));
+    menuBackground.setOutlineColor(sf::Color(100, 150, 100, 150));
+    menuBackground.setOutlineThickness(3.f);
+    menuBackground.setPosition(700.f, 400.f);
+
     std::vector<MenuItem> pauseMenu = {
-        MenuItem(L"МЕНЮ", font, 50, {1000.f, 250.f}, [&gameState]() {
-                gameState = GAME;
-            }, true),
-        MenuItem(L"Продолжить", font, 36, {100.f, 550.f}, []() {}, false),
-        MenuItem(L"Выйти", font, 36, {100.f, 200.f}, []() {}, false)
+        MenuItem(L"МЕНЮ", font, 50, {750.f, 450.f}, []() {}, true),
+        MenuItem(L"Продолжить", font, 36, {710.f, 500.f}, [&gameState]() {gameState = GAME;}, false),
+        MenuItem(L"Выйти", font, 36, {725.f, 600.f}, [&window]() {window->close();}, false) // выйти в лаунчер
     };
     bool menuActive = true;
 
     while (window->isOpen() && menuActive) {
-        window->clear(sf::Color(30, 30, 30)); //изначальный фон
         sf::Event event;
         while (window->pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window->close();
                 menuActive = false;
             }
-        }
-        if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-            gameState = GAME;
-            menuActive = false;
-        }
-        if (event.type == sf::Event::MouseButtonPressed &&
-            event.mouseButton.button == sf::Mouse::Left) {
-            for (auto& item : pauseMenu) {
-                if (item.isMouseOver(*window)) {
-                    item.onClick();
-                    if (gameState == GAME) {
-                        menuActive = false;
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                gameState = GAME;
+                menuActive = false;
+            }
+            if (event.type == sf::Event::MouseButtonPressed &&
+                event.mouseButton.button == sf::Mouse::Left) {
+                for (auto& item : pauseMenu) {
+                    if (item.isMouseOver(*window)) {
+                        item.onClick();
+                        if (gameState == GAME) {
+                            menuActive = false;
+                        }
+                        break;
                     }
-                    break;
                 }
             }
         }
@@ -88,9 +99,10 @@ void renderPause(sf::RenderWindow* window, EnumGameState& gameState) {
             item.hovered = item.isMouseOver(*window);
             item.update(time);
         }
-        window->clear(sf::Color(30, 30, 30));
 
+        window->draw(backgroundSprite);
         window->draw(overlay);
+        window->draw(menuBackground);
 
         for (const auto& item : pauseMenu) {
             window->draw(item.text);
