89007bded0257b46507cfeb522f42a9580dc317c
Максим Колмогоров <maksim28062006@mail.ru>
2025-11-06 18:24:23 +0300

    Начал реализацию меню паузы

--- My_Game_TPro/pauseMenu.h
+++ My_Game_TPro/pauseMenu.h
@@ -1,2 +1,22 @@
 #pragma once
 //то что видно на паузе
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <cmath> // для sin
+
+class MenuItem;
+
+class MenuItem {
+public:
+    sf::Text text;
+    std::function<void()> onClick;
+    bool title;
+    bool hovered = false;
+    MenuItem(const sf::String& label, sf::Font& font, unsigned int size, const sf::Vector2f& pos, std::function<void()> callback, bool title);
+    bool isMouseOver(const sf::RenderWindow& window) const;
+    void update(float time);
+    bool gettitle() const;
+};
