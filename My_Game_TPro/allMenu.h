5d8a0e1fc873b60314f2200bebd2e9a0bec7b858
Максим Колмогоров <maksim28062006@mail.ru>
2025-11-11 20:59:47 +0300

    правки header

--- My_Game_TPro/allMenu.h
+++ My_Game_TPro/allMenu.h
@@ -1,11 +1,10 @@
 #pragma once
-//то что видно на паузе
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
 #include <vector>
 #include <functional>
-#include <cmath> // для sin
+#include <cmath>
 #include "game.h"
 #include <chrono>
 
@@ -13,17 +12,18 @@ class MenuItem;
 class AdTimer;
 
 
+//свой таймер для рекламы -_-
 class AdTimer {
 private:
     std::chrono::steady_clock::time_point lastAdTime;
     unsigned int adDelaySeconds;
 
 public:
-    AdTimer(unsigned int delaySeconds = 300);
-    bool canShowAd() const;
-    void recordAdShown();
-    unsigned int getRemainingSeconds() const;
-    void setDelay(unsigned int delaySeconds);
+    AdTimer(unsigned int delaySeconds = 300); //5 минут ждать, эх(
+    bool canShowAd() const; //5 минут прошло?
+    void recordAdShown(); //считаааем
+    unsigned int getRemainingSeconds() const; //снова считаеем
+    void setDelay(unsigned int delaySeconds); //выставляем тайм
 };
 
 class MenuItem {
@@ -32,11 +32,11 @@ public:
         const sf::Vector2f& pos, std::function<void()> callback,
         bool title = false,
         const sf::Color& normalColor = sf::Color(180, 255, 180),
-        const sf::Color& hoverColor = sf::Color(0, 255, 0));
+        const sf::Color& hoverColor = sf::Color(0, 255, 0)); // constructoor
 
-    bool isMouseOver(const sf::RenderWindow& window) const;
-    void update(float time);
-    bool gettitle() const;
+    bool isMouseOver(const sf::RenderWindow& window) const; // по базе себя чувствует
+    void update(float time); //навели
+    bool gettitle() const; //использовал ли вообще?:
 
     sf::Text text;
     bool hovered = false;
@@ -48,6 +48,7 @@ private:
     sf::Color hoverColor;
 };
 
+//ну тут очевидно вроде кто и где
 void renderPause(sf::RenderWindow* window, EnumGameState& gameState, std::function<void(sf::RenderWindow*)> drawStack, AdTimer& adTimer);
 void renderWin(sf::RenderWindow* window, EnumGameState& gameState, uint8_t Level, std::function<void(sf::RenderWindow*)> drawStack);
 void renderLose(sf::RenderWindow* window, EnumGameState& gameState, uint8_t Level, std::function<void(sf::RenderWindow*)> drawStack);
