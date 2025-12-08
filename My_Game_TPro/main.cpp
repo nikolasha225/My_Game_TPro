995ce64290d2ccb0aa94e8c19ef0a3ffab064ea5
nikolasha225 <nikolasha225@yandex.ru>
2025-12-08 23:53:14 +0300

    критикал правки

--- My_Game_TPro/main.cpp
+++ My_Game_TPro/main.cpp
@@ -8,14 +8,15 @@ int main(int argc, char* argv[])
     //������� ��������� 
     uint8_t __difficult = 1;
     unsigned __id = 0;
-    uint8_t __level = 1;
+    //uint8_t __level = 1;
+    LEVEL = 1;
 
     if (argc > 1) __difficult = static_cast<uint8_t>(std::stoi(argv[1]));
     if (argc > 2) __id = static_cast<unsigned>(std::stoi(argv[2]));
-    if (argc > 3) __level = static_cast<bool>(std::stoi(argv[3]));
+    if (argc > 3) LEVEL = static_cast<uint8_t>(std::stoi(argv[3]));
     if (argc > 4) VOLUMEI = static_cast<bool>(std::stoi(argv[4]));
 
-    LEVEL = __level;
+    //LEVEL = 2;
 
     // SF ������� �����������
     sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
@@ -83,7 +84,7 @@ int main(int argc, char* argv[])
         std::string command = std::string("\"") + exePath + "\" " +
             std::to_string(__difficult) + " " +
             std::to_string(__id) + " " +
-            std::to_string(LEVEL);+ " " +
+            std::to_string(LEVEL) + " " +
             std::to_string(VOLUMEI);
 
         system(("start \"\" /B " + command + " >nul 2>&1").c_str());
@@ -113,9 +114,8 @@ int main(int argc, char* argv[])
             {
                 TOWER_MANAGER.checkEvents(&window);
             }
-        }
-
-            if (OBJ_STACK.getCountOf(enemy) == 0 && TIME == 0 || (EVENT.type == sf::Event::KeyPressed && EVENT.key.code == sf::Keyboard::W)) {
+        
+            if ((OBJ_STACK.getCountOf(enemy) == 0 && TIME == 0) || (EVENT.type == sf::Event::KeyPressed && EVENT.key.code == sf::Keyboard::W)) {
                 if(LEVEL == 3)
                     GAME_STATE = OVER;
                 else
@@ -124,7 +124,7 @@ int main(int argc, char* argv[])
             }
             if (HEALTH <= 0 || (EVENT.type == sf::Event::KeyPressed && EVENT.key.code == sf::Keyboard::L))
                 GAME_STATE = LOSE;
-        
+        }
 
         //##########CLEAR####################
 
